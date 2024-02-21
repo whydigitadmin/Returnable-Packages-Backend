206af0bdc079525971c107f0ec790ea5f9ee2c7c
vasanth <vasanth@whydigit.com>
2024-02-21 16:29:29 +0530

    update and delete api for customers bank details address details

--- src/main/java/com/whydigit/efit/entity/CustomersBankDetailsVO.java
+++ src/main/java/com/whydigit/efit/entity/CustomersBankDetailsVO.java
@@ -5,8 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.whydigit.efit.dto.CreatedUpdatedDate;
 
 import lombok.AllArgsConstructor;
@@ -27,6 +30,13 @@ public class CustomersBankDetailsVO {
 	private String ifscCode;
 	private String branch;
 	private Long accountNo;
+	private boolean isDefault;
+
+	@ManyToOne
+	@JoinColumn(name = "customers_id")
+	@JsonBackReference
+	private CustomersVO customersVO;
+
 	@Embedded
 	private CreatedUpdatedDate commonDate = new CreatedUpdatedDate();
 }
--- src/main/java/com/whydigit/efit/repo/CustomersBankDetailsRepo.java
+++ src/main/java/com/whydigit/efit/repo/CustomersBankDetailsRepo.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.whydigit.efit.entity.CustomersBankDetailsVO;
 
-public interface CustomersBankDetailsRepo  extends JpaRepository<CustomersBankDetailsVO, Long>{
+public interface CustomersBankDetailsRepo extends JpaRepository<CustomersBankDetailsVO, Long> {
 
 }
