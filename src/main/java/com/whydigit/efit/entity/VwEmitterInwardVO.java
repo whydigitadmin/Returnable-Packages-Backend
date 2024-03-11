e51b433b5f8e229f37df81bc154813f198c95866
vasanth <vasanth@whydigit.com>
2024-03-11 10:50:30 +0530

    error fix

--- src/main/java/com/whydigit/efit/entity/VwEmitterInwardVO.java
+++ src/main/java/com/whydigit/efit/entity/VwEmitterInwardVO.java
@@ -17,7 +17,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class VwEmitterInwardVO {
 @Id
-private Long requestID;
+private Long requestid;
 private Long orgId;
 private LocalDate requestedDate;
 private LocalDate demandDate;
