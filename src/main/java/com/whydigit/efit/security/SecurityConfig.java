3ebb70a6763bfdae578b20da78224698b8c8494a
Aravinth <justinaravinth@whydigit.com>
2024-03-11 19:43:18 +0530

    Get Emitter by Warehouseid

--- src/main/java/com/whydigit/efit/security/SecurityConfig.java
+++ src/main/java/com/whydigit/efit/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig {
 						"/api/partStudy/stockDetail/{id}", "/api/emitter/createIssueRequest", "/api/emitter/*",
 						"/api/auth/createUser", "/api/master/flow/getFlowByIds", "/api/master/getAllFlowName",
 						"/api/emitter/getViewEmitterByWarehouse", "/api/emitter/getViewEmitter","/api/auth/changePassword",
-						"/api/emitter/getViewEmitterByFlow","/api/emitter/viewEmitterInward","/api/warehouse/getWarehouseByUserID")
+						"/api/emitter/getViewEmitterByFlow","/api/emitter/viewEmitterInward","/api/warehouse/getWarehouseByUserID","/api/emitter/getemitterByWarehouseId")
 				.permitAll().antMatchers("/api/**").hasAnyRole("USER", "GUEST_USER").anyRequest().authenticated();
 
 		http.addFilterBefore(tokenAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
