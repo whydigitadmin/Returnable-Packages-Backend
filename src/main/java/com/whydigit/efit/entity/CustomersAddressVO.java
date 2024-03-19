ec63c22d16ce64415c6827c1f0186303ce028a10
vasanth <vasanth@whydigit.com>
2024-03-19 17:56:30 +0530

    flow datastrructure changes

--- src/main/java/com/whydigit/efit/entity/CustomersAddressVO.java
+++ src/main/java/com/whydigit/efit/entity/CustomersAddressVO.java
@@ -1,7 +1,6 @@
 package com.whydigit.efit.entity;
 
 import javax.persistence.Column;
-import javax.persistence.Embedded;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,7 +11,6 @@ import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.whydigit.efit.dto.CreatedUpdatedDate;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
